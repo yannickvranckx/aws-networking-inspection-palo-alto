847328fe0dec2a3ffa52e670d7488fff58c909cc
Yannick Vranckx <41998248+yannickvranckx@users.noreply.github.com>
2024-11-05 11:24:06 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,9 +5,5 @@ locals {
   post_inspection_rt          = "tgw-rtb-12345"    # RT ID post inspection
   peering_rt                  = "tgw-rtb-12345"    # RT ID peering
   dx_gw                       = "tgw-attach-12345" # DX Gateway ID
-  smi_dev_attach              = "tgw-attach-12345" # smi dev attach id
-  smi_dev_cidr                = "x.x.x.x/x"               # smi dev cidr
-  smi_prd_attach              = "tgw-attach-12345" # smi prd attach id
-  smi_prd_cidr                = "x.x.x.x/x"                # smi prd cidr
-  smi_tst_attach              = "tgw-attach-12345" # smi tst attach id
-  smi_tst_cidr                = "10.194.254.0/24"              # smi tst cidr
+  attachment_tgw              = "tgw-attach-12345" # smi dev attach id
+  attachment_tgw_cidr                = "x.x.x.x/x"               # smi dev cidr
